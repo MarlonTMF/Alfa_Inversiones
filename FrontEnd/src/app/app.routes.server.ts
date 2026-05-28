5e958fa1e2060e1da1cc03c5f1245ce9760e7670
MarlonTMF <marlontomasmarzofernandez@gmail.com>
2026-05-28 12:45:41 -0400

    Pre Front Deppp

--- FrontEnd/src/app/app.routes.server.ts
+++ FrontEnd/src/app/app.routes.server.ts
@@ -2,31 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'analisis/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'admin/proyectos/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'admin/proyectos/:id/editar',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'admin/validar-terreno/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'inversor/inversion-detalle',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'explorar/proyecto/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
